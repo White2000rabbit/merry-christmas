84749a5a42184b692c905fd99cb5cabec3a3cebd
Alyona git config --global user.email alena_novour@mail.ru <alena_novour@mail.ru>
2023-11-02 19:52:01 +0300

    change

--- CardImage.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function CardImage() {
-  return React.createElement(
-    "li",
-    { className: "card" },
-    React.createElement("img", { src: "img/photo-card-1.jpg", width: "185", height: "100", alt: "" })
-  );
-}
---
+++ src/data.jsx
@@ -0,0 +1,81 @@
+const words = [
+{
+   id: '1',
+   token: '1',
+   word: 'Первое декабря',
+   type: 'word'
+ },
+ {
+   id: '2',
+   token: '1',
+   url: 'img/photo-card-4.jpg',
+   type: 'image'
+ },
+ {
+   id: '3',
+   token: '2',
+   word: 'Второе декабря',
+   type: 'word'
+ },
+ {
+   id: '4',
+   token: '2',
+   url: 'img/photo-card-6.jpg',
+   type: 'image'
+ },
+ {
+   id: '5',
+   token: '3',
+   word: 'Третье декабря',
+   type: 'word'
+ },
+ {
+   id: '6',
+   token: '3',
+   url: 'img/photo-card-5.jpg',
+   type: 'image'
+ },
+ {
+   id: '7',
+   token: '4',
+   word: 'Четвертое декабря',
+   type: 'word'
+ },
+ {
+   id: '8',
+   token: '4',
+   url: 'img/photo-card-2.jpg',
+   type: 'image'
+ },
+ {
+   id: '9',
+   token: '5',
+   word: 'Пятое декабря',
+   type: 'word'
+ },
+ {
+   id: '10',
+   token: '5',
+   url: 'img/photo-card-1.jpg',
+   type: 'image'
+ },
+ {
+   id: '11',
+   token: '6',
+   word: 'Шестое декабря',
+   type: 'word'
+ },
+ {
+   id: '12',
+   token: '6',
+   url: 'img/photo-card-3.jpg',
+   type: 'image'
+ }
+];
+
+const selectedItems = ['3', '4'];
+const finishedItems = ['2', '5'];
+
+window.words = words;
+window.selectedItems = selectedItems;
+window.finishedItems = finishedItems;
